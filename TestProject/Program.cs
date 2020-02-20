b149a60b00262e05d32c54b4af254f37da031bd5
tpichurina <krushinskayatanya@gmail.com>
2020-02-20 22:32:50 +0200

    game

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,94 +7,63 @@ namespace TestProject
 
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello World!");
-            
-            
-            Console.ReadLine();
+            // int a = 5;
+            // int b = 10;
+            // bool isOnline = false;
 
-            Student std1;
+            // if (isOnline)
+            //  {
+            //     Console.WriteLine("Is Online");
+            //   }
+            //  else
+            // {
+            //      Console.WriteLine("Is Offline");
+            // }
 
-            std1 = new Student();
-            Console.WriteLine(std1.age);
+            /*Console.WriteLine("Enter you name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter you age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine($"{name} you are adalt");
+            }
+            else
+            {
+                Console.WriteLine($"{name} you are still child");
+            }
+            */
+         /* int a = 0;
+            while (a<10)
+            {
+                
+                Console.WriteLine("Enter you age");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            */
 
-            Console.ReadLine();
-            std1.age = 25;
-            Console.WriteLine(std1.age);
-            Console.ReadLine();
+            Console.WriteLine("Hello Player1");
+            Console.WriteLine("Propose number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+          
+            Console.WriteLine("Hello Player2");
+            Console.WriteLine("Guess number, you have 5 attampts");
+           int answer = Convert.ToInt32(Console.ReadLine());
+            int countAttamps = 0;
+            while (countAttamps < 5)
+            {
+                if (answer != number)
+                {
+                    Console.WriteLine("Sorry, try againe");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("You are win");
 
-            Calculate(std1);
-            Console.WriteLine(std1.age);
-            string name = "Tanya";
-            std1.SetName(name);
-            std1.ShowName();
-            Console.ReadLine();
-
-            string Fname = "Tanya";
-            int age = 31;
-            bool progremer = true;
-            Console.WriteLine($"Name: {Fname}");
-            Console.WriteLine($"Age: {age}");
-            Console.WriteLine($"Programmer: {progremer}");
-
-            var message = new Student();
-            var t = 1000.77;
-            Console.WriteLine(t);
-            Console.WriteLine(message.ToString());
-
-           // string namem = "Tanya";
-           // int age5 = 31;
-            //double height = 1.7;
-            //Console.WriteLine($"Name: {namem} Age {4+7}year Height {height}m");
-
-            string namem = "Tanya";
-            int age5 = 31;
-            double height = 1.7;
-            Console.WriteLine("Name: {0} Age {1}year Height {2}m", namem, age5, height);
-
-            Console.WriteLine("Enter name of your cat: ");
-            string catname = Console.ReadLine();
-            Console.WriteLine($"You so cute {catname} ;-)");
-
-            Console.Write("Enter name: ");
-            string name1 = Console.ReadLine();
-
-            Console.Write("Enter age: ");
-            int age1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter cat age: ");
-            double catage = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Name: {name1}, Age: {age1}, Cat Age: {catage}");
-        
-
-
-
-
-
-
-
+                }
+            }
         }
-        static void Calculate(Student s)
-        {
-            s.age += 1;
-        }
-
-    }
-
-    public class Student
-    {
-        public int age;
-        private string name;
-
-        public void SetName(string name)
-        {
-            this.name = name;
-        }
-
-        public void ShowName()
-        {
-            Console.WriteLine(name);
-        }
-
     }
 }
