f5f713292bd3f8cf7c384ccf690270a45e016779
tpichurina <krushinskayatanya@gmail.com>
2020-02-26 20:49:00 +0200

    while

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,73 +7,23 @@ namespace TestProject
 
         static void Main(string[] args)
         {
-            // int a = 5;
-            // int b = 10;
-            // bool isOnline = false;
 
-            // if (isOnline)
-            //  {
-            //     Console.WriteLine("Is Online");
-            //   }
-            //  else
-            // {
-            //      Console.WriteLine("Is Offline");
-            // }
-
-            /*Console.WriteLine("Enter you name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter you age");
-            int age = Convert.ToInt32(Console.ReadLine());
-            if (age >= 18)
+            Console.WriteLine("Please enter your number");
+            double number = Convert.ToDouble(Console.ReadLine());
+            while (number>0)
             {
-                Console.WriteLine($"{name} you are adalt");
-            }
-            else
-            {
-                Console.WriteLine($"{name} you are still child");
-            }
-            */
-         /* int a = 0;
-            while (a<10)
-            {
+                Console.WriteLine(--number);
                 
-                Console.WriteLine("Enter you age");
-                a = Convert.ToInt32(Console.ReadLine());
-            }
-            */
-
-            Console.WriteLine("Hello Player1");
-            Console.WriteLine("Propose number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-          
-            Console.WriteLine("Hello Player2");
-            Console.WriteLine("Guess number, you have 5 attampts");
-           
-            int countAttamps = 0;
-            while (countAttamps < 5)
-            {
-                int answer = Convert.ToInt32(Console.ReadLine());
-                if (answer != number)
+                if (number==0)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Sorry, try againe");
-                    countAttamps = countAttamps + 1;
-                    if (countAttamps == 5)
-                    {
-                      Console.Clear();
-                      Console.WriteLine("you are looser");
-                    }
+                    Console.WriteLine("Boom");
+                    
+                   
                 }
-                else
-                {
-                    Console.WriteLine("You are win");
-                    countAttamps = 5;
-                }
-
+              
             }
 
-           
         }
     }
 }
+
